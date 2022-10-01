7d27cee601d5212162e27874692b222eaab0051b
Abu-Lut <muimkh123@gmail.com>
2022-10-01 22:47:41 +0530

    added last black tile check functionality

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const gameContainer = document.querySelector('#gameContainer')
 console.log(gameContainer)
 tileNum = 400
-
+let gameOver = false
 
 // Creating tiles
 
 for (let i=0; i<tileNum; i++) {
     let tile = document.createElement('div')
     tile.classList.add('tile')
-    i<4 && tile.classList.add('end')
+    i<4 && tile.classList.add('end') 
+    i<4 && tile.classList.remove('tile')
     gameContainer.appendChild(tile)
 }
 
@@ -30,7 +31,6 @@ for (let i=1; i<tileList.length+1; i++){
     
     if (i%4==1){
         randomTile = randomGen()
-        console.log(randomTile)
     }
 
     if ((i%4)==randomTile){
@@ -42,15 +42,16 @@ for (let i=1; i<tileList.length+1; i++){
 
 const activeTileList = document.querySelectorAll('.active')
 
+const activeList = Array.from(document.querySelectorAll('.active'))
 // Clicking an 'active' tile will make it inactive
 
 for (let tile of activeTileList){
     tile.addEventListener('click', () => {
         tile.classList.remove('active')
         tile.classList.add('inactive')
+        activeList.pop()
     })
 }
-
 
 // Scrolls all the way to the bottom
 gameContainer.scrollBy(0, 150*(tileNum/4))
@@ -60,10 +61,14 @@ gameContainer.scrollBy(0, 150*(tileNum/4))
 gameContainer.addEventListener("mousewheel", e => e.preventDefault())
 
 
-
 function isInViewport(element) {
     const rect = element.getBoundingClientRect();
-    return rect.top >= 0
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    )
 }
 
 
@@ -71,31 +76,56 @@ const endTile = document.querySelector('.end')
 
 let accelerator = 1
 
-let scrollInterval = setInterval(() => {
-    if (isInViewport(endTile)) {
-        clearInterval(scrollInterval)
-    }
-    else {
-        gameContainer.scrollBy(0, -(accelerator))
-    }
-    // console.log(isInViewport(endTile))
-    // console.log(accelerator)
-}, 10)
+const lastTile = activeList.slice(-1)[0]
+lastTile.textContent += '\n start'
+
+
+lastTile.addEventListener('click', () => {
+    let scrollInterval = setInterval(() => {
+        if (isInViewport(endTile)) {
+            clearInterval(scrollInterval)
+        }
+        else {
+            gameContainer.scrollBy(0, -(accelerator))
+        }
+        // console.log(isInViewport(endTile))
+        // console.log(accelerator)
+    }, 10)
+})
+
+
 
 let acceleratorInterval = setInterval(() => {
     if (isInViewport(endTile)){
         clearInterval(acceleratorInterval)
     }
     else{
-        accelerator +=0.001, 10
+        accelerator +=0.001
     }
-})
+}, 10)
 
 // setInterval(() => {
 //     gameContainer.scrollBy(0, -2)
 //     console.log(accelerator)
 // }, 10)
 
+
+// Checking if an black tile is missed
+
+
+let missingInterval = setInterval(() => {
+    if (isInViewport(endTile)){
+        clearInterval(missingInterval)
+    }
+    else{
+        console.log(isInViewport(activeList.slice(-1)[0]))
+        if (!isInViewport(activeList.slice(-1)[0])){
+            gameOver = true
+            clearInterval(missingInterval)
+            console.log("GAME OVER")
+        }
+    }
+}, 10)
 
 
 
