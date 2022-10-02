04cdb2da4e45db1567cea33be4e0cf2f6743515d
Abu-Lut <muimkh123@gmail.com>
2022-10-02 20:48:02 +0530

    trying to fix missing tile functionality

--- index.js
+++ index.js
@@ -49,115 +49,142 @@ function gameCreator(){
     const activeTileList = document.querySelectorAll('.active')
 
     const activeList = Array.from(document.querySelectorAll('.active'))
+
+    let visibleTiles = []
     // Clicking an 'active' tile will make it inactive
 
-for (let tile of activeTileList){
-    tile.addEventListener('click', () => {
-        increaseScore()
-        tile.classList.remove('active')
-        tile.classList.add('inactive')
-        activeList.pop()
+    for (let tile of activeTileList){
+        tile.addEventListener('click', () => {
+            increaseScore()
+            tile.classList.remove('active')
+            tile.classList.add('inactive')
+            visibleTiles.shift()
+            activeList.pop()
+        })
+    }
+
+    // Scrolls all the way to the bottom
+    gameContainer.scrollBy(0, 150*(tileNum/4))
+
+
+    // Preventing scroll wheel action inside game container
+    gameContainer.addEventListener("mousewheel", e => e.preventDefault())
+
+
+    function isInViewport(element) {
+        const rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        )
+    }
+
+
+    const endTile = document.querySelector('.end')
+
+    let accelerator = 1
+
+    const lastTile = activeList.slice(-1)[0]
+    lastTile.innerHTML += '<span>start</span>'
+
+    let scrollInterval;
+
+    lastTile.addEventListener('click', () => {
+        scrollInterval = setInterval(() => {
+            if (isInViewport(endTile)) {
+                clearInterval(scrollInterval)
+            }
+            else {
+                gameContainer.scrollBy(0, -(accelerator))
+                accelerator +=0.001
+            }
+            // console.log(isInViewport(endTile))
+            // console.log(accelerator)
+        }, 10)
     })
-}
-// Scrolls all the way to the bottom
-gameContainer.scrollBy(0, 150*(tileNum/4))
+
+    // let acceleratorInterval = setInterval(() => {
+    //     if (isInViewport(endTile)){
+    //         clearInterval(acceleratorInterval)
+    //     }
+    //     else{
+            
+    //     }
+    // }, 100)
+
+    // setInterval(() => {
+    //     gameContainer.scrollBy(0, -2)
+    //     console.log(accelerator)
+    // }, 10)
 
 
-// Preventing scroll wheel action inside game container
-gameContainer.addEventListener("mousewheel", e => e.preventDefault())
+    function increaseScore(){
+        scoreCount +=1
+        let score = document.getElementById("score")
+        score.textContent = scoreCount
+        // let highScore = 0
 
-
-function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    )
-}
-
-
-const endTile = document.querySelector('.end')
-
-let accelerator = 1
-
-const lastTile = activeList.slice(-1)[0]
-lastTile.innerHTML += '<span>start</span>'
-
-let scrollInterval;
-
-lastTile.addEventListener('click', () => {
-    scrollInterval = setInterval(() => {
-        if (isInViewport(endTile)) {
-            clearInterval(scrollInterval)
-        }
-        else {
-            gameContainer.scrollBy(0, -(accelerator))
-        }
-        // console.log(isInViewport(endTile))
-        // console.log(accelerator)
-    }, 10)
-})
-
-let acceleratorInterval = setInterval(() => {
-    if (isInViewport(endTile)){
-        clearInterval(acceleratorInterval)
     }
-    else{
-        accelerator +=0.001
-    }
-}, 10)
 
-// setInterval(() => {
-//     gameContainer.scrollBy(0, -2)
-//     console.log(accelerator)
-// }, 10)
+    // GAME OVER POPUP
 
-
-// Checking if an black tile is missed
-
-
-let missingInterval = setInterval(() => {
-    if (isInViewport(endTile)){
+    function stopGame() {
+        gameoverCard.classList.add("openPopup");
+        clearInterval(scrollInterval);
+        // clearInterval(acceleratorInterval);
         clearInterval(missingInterval)
     }
-    else{
-        if (!isInViewport(activeList.slice(-1)[0])){
-            gameOver = true
+
+    let whiteTiles = document.querySelectorAll(".tile");
+    let gameoverCard = document.querySelector(".gameover-popup");
+
+    whiteTiles.forEach((item) => item.addEventListener("click", ()=>{
+        item.style.backgroundColor = "red";
+        stopGame()
+    }))
+
+
+    // Checking if an black tile is missed
+
+
+    let options = {
+        root: gameContainer,
+        rootMargin: '0px',
+        threshold: 1.0
+      }
+      
+    let observer = new IntersectionObserver(()=> {
+        console.log(activeList.slice(-1)[0])
+        visibleTiles.push(activeList.slice(-1)[0])
+        activeList.pop()
+    }, options);
+
+      
+    let missingInterval = setInterval(() => {
+        if (isInViewport(endTile)){
             clearInterval(missingInterval)
-            console.log("GAME OVER")
+            console.log("YOU HAVE WON")
         }
-    }
-}, 10)
+        else{
+            observer.observe(activeList.slice(-1)[0])
+            
+            if (visibleTiles.length>1 && !isInViewport(visibleTiles[0])){
+                console.log(visibleTiles)
+                stopGame()
+            }
+        }
+    }, 10)
 
-function increaseScore(){
-    scoreCount +=1
-    let score = document.getElementById("score")
-    score.textContent = scoreCount
-    // let highScore = 0
-    
-}
 
-// GAME OVER POPUP
 
-let whiteTiles = document.querySelectorAll(".tile");
-let gameoverCard = document.querySelector(".gameover-popup");
-
-whiteTiles.forEach((item) => item.addEventListener("click", ()=>{
-    item.style.backgroundColor = "red";
-    // gameoverCard.style.display = "flex";
-    gameoverCard.classList.add("openPopup");
-    clearInterval(scrollInterval);
-    clearInterval(acceleratorInterval);
-}))
-
-document.getElementById("restart").addEventListener("click", ()=>{
-    console.log("hi")
-    gameoverCard.classList.remove("openPopup");
-    gameContainer.innerHTML = "";
-    gameCreator();
-})
+    document.getElementById("restart").addEventListener("click", ()=>{
+        gameoverCard.classList.remove("openPopup");
+        gameContainer.innerHTML = "";
+        gameContainer.appendChild(gameoverCard)
+        gameCreator();
+    })
 }
 
 gameCreator();
